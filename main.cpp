995f19384a31b5f6970a3ddf5ccde98f26596deb
Adiboy3112 <adiboy_3112@kgpian.iitkgp.ac.in>
2022-08-30 21:13:25 +0530

    The Dominance equilibrium works

--- main.cpp
+++ main.cpp
@@ -3,7 +3,43 @@
 using namespace std;
 int rows=4;
 int columns=2;
-vector<int> ans;
+
+vector<vector<int>> wds(columns);
+vector<vector<int>> vwds(columns);
+vector<vector<int>> comb;
+
+void combinations(vector<vector<int> > array, int i, vector<int> accum)
+{
+    if (i == array.size()) // done, no more rows
+    {
+        comb.push_back(accum); // assuming comb is global
+    }
+    else
+    {
+        vector<int> row = array[i];
+        for(int j = 0; j < row.size(); ++j)
+        {
+            vector<int> tmp(accum);
+            tmp.push_back(row[j]);
+            combinations(array,i+1,tmp);
+        }
+    }
+}
+
+void displaystrategies()
+{
+    for (int i = 0; i < comb.size(); i++)
+    {
+        for (int j = 0; j < comb[i].size(); j++)
+        {
+            cout<<comb[i][j]<<" ";
+        }
+        comb[i].clear();
+        cout<<endl;
+    }
+    comb.clear();
+    
+}
 
 int SDS(int player, int u[4][2],vector<int> s)
 {
@@ -120,7 +156,7 @@ void WDS(int player, int u[4][2],vector<int> s)
        {
             if(set1.count(i)==x&&v[i]==1)
             {
-                ans.push_back(i);
+                wds[player].push_back(i);
                
             }
         
@@ -183,7 +219,7 @@ void VWDS(int player, int u[4][2],vector<int> s)
        {
             if(set1.count(i)==x)
             {
-                ans.push_back(i);
+                vwds[player].push_back(i);
                
             }
         
@@ -193,21 +229,109 @@ void VWDS(int player, int u[4][2],vector<int> s)
 
 
 
+
+
 int main()
 {
 
-    int u[8][2];
-    vector<int>s(2);
+    int u[8][2],sds[columns];
+    vector<int>s(columns);
     s[0]=2;
     s[1]=2;
     u[0][0]=3;
     u[0][1]=1;
-    u[1][0]=2;
+    u[1][0]=1;
     u[1][1]=2;
-    u[2][0]=0;
-    u[2][1]=0;
+    u[2][0]=4;
+    u[2][1]=1;
     u[3][0]=2;
     u[3][1]=2;
-    WDS(0,u,s);
-    cout<<ans.size()<<endl;
+    for (int i = 0; i < columns; i++)
+    {
+        sds[i]=SDS(i,u,s);
+        WDS(i,u,s);
+        VWDS(i,u,s);
+    
+    }
+    // check for SDSE
+    bool SDSE=true;
+    vector<int>sdse;
+    for (int i = 0; i < columns; i++)
+    {
+        if(sds[i]==-1)
+        {
+            SDSE=false;
+            break;
+
+        }
+        else sdse.push_back(sds[i]);
+
+    }
+    if(SDSE)
+    {
+        cout<<"The SDSE for this game exists for the strategy: ";
+        for (int i = 0; i < columns; i++)
+        {
+           cout<<sdse[i]<<" ";
+
+        }
+        cout<<endl;
+        
+    }
+    
+    else
+    {
+        cout<<"The SDSE for this game dosen't exist"<<endl;
+    }
+
+    // check for WDSE
+    bool WDSE=true;
+
+    for(int i = 0; i < columns; i++)
+    {
+    /* code */
+        if(wds[i].empty())
+        {
+            WDSE=false;
+            break;
+        }   
+    }
+    if(WDSE)
+    {
+        cout<<"The WDSE for this game exists for the strategy: \n";
+        vector<int> acum;
+        combinations(wds,0,acum);
+        displaystrategies();
+    }
+    else 
+    {
+        cout<<"The WDSE for this game dosen't exist"<<endl;
+    }
+
+    // check for VWDSE
+    bool VWDSE=true;
+
+    for(int i = 0; i < columns; i++)
+    {
+    /* code */
+        if(vwds[i].empty())
+        {
+            VWDSE=false;
+            break;
+        }   
+    }
+    if(VWDSE)
+    {
+        cout<<"The VWDSE for this game exists for the strategy: \n";
+        vector<int> acum;
+        combinations(vwds,0,acum);
+        displaystrategies();
+    }
+    else
+    {
+        cout<<"The VWDSE for this game dosen't exist"<<endl;
+    }
+
+
+    
 }
